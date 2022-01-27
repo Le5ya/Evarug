755d1b51d9771469ffcbe601500ee738eec123e4
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2022-01-27 23:16:30 +0300

    fixing

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 3,
   loop: true,
   speed: 400,
-  spaceBetween: 70,
+  spaceBetween: 50,
   centeredSlides: true,
   slideToClickedSlide: true,
   toggle: true,
