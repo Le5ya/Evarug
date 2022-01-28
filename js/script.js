2bbfbf1c85255e7e389c2316ed1c357890820dbd
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2022-01-28 18:49:18 +0300

    modal form

--- js/script.js
+++ js/script.js
@@ -7,13 +7,47 @@ const swiper = new Swiper('.swiper', {
   slideToClickedSlide: true,
   toggle: true,
   breakpoints: {
+
       1100: {
       slidesPerView: 2
     },
    
-    768: {
+    400: {
       slidesPerView: 1
     }
   }
+ 
+
+});
+
+ const modalWindow = document.querySelector('.modal');
+ const btnDisplay = document.querySelectorAll('.main-display__button')[0];
+ const btnModal = document.querySelectorAll('.modal__button')[0];
+//  btnModal.style.opacity = 0;
+
+document.addEventListener('click', () => {
+  return;
+  modalWindow.classList.add('active');
+});
+document.addEventListener('dblclick', () => {
+  return;
+  modalWindow.classList.remove('active');
+});
+
+btnDisplay.addEventListener('click', () => {
+  // console.log('CLICK')
+  modalWindow.classList.add('active');
+});
+
+btnModal.addEventListener('click', () => {
+  modalWindow.classList.remove('active');
+});
+
+modalWindow.addEventListener('click', (e) => {
+  const isModal = e.target.closest('.modal__inner');
+  if(!isModal) {
+    modalWindow.classList.remove('active'); 
+  }
+  
 
 });
